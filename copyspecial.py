8f406d01eeeb10b556dc44aa4356a8b1e3109d8d
Ruth Mayodi <ruthmayodi@gmail.com>
2020-06-08 13:54:34 -0400

    assignment complete

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Ruth Mayodi, ruthmayodi with the help of Howard Post"
 
 import re
 import os
@@ -19,17 +19,39 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    new_list = [os.path.abspath(os.path.join(os.getcwd(), f))
+        for f in (os.listdir(dirname))
+        if re.search(r'__(\w+)__', f)]
+    return new_list
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
+    """Given a list files, copies to a new directory."""
+    try:
+        os.makedirs(dest_dir)
+    except OSError as e:
+        print (e)
+        exit(1)
+    for path in path_list:
+        filename = os.path.basename(path)
+        current_path = os.path.dirname(path)
+        abs_path = os.path.join(current_path, dest_dir, filename)
+        shutil.copyfile(path, abs_path)
     return
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
+    """Given a list of files adds them to a zip"""
+    list_file = ''
+    for path in path_list:
+        list_file += path + ' '
+    print("Command I'm going to do:")
+    print('zip -j', list_file)
+    try:
+        subprocess.call(['zip', '-j', dest_zip] +path_list)
+    except OSError as e:
+        print(e)
+        exit(1)
     return
 
 
@@ -39,18 +61,20 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help= 'directory to search for files')
     ns = parser.parse_args(args)
 
-    # TODO: you must write your own code to get the command line args.
-    # Read the docs and examples for the argparse module about how to do this.
-
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation. If something is wrong (or missing) with
-    # any required args, the general rule is to print a usage message and
-    # exit(1).
-
-    # Your code here: Invoke (call) your functions
+    if not ns:
+        parser.print_usage()
+        sys.exit(1)
+    if ns.todir:
+        copy_to(get_special_paths(ns.from_dir), ns.todir)
+    elif ns.tozip:
+        zip_to(get_special_paths(ns.from_dir), ns.todir)
+    else:
+        path_list = get_special_paths(ns.from_dir)
+        for path in path_list:
+            print(path)
 
 
 if __name__ == "__main__":
